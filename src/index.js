78990af6147dd48d6a62e2ce37ff82863660dbd4
mdvacula <mdvacula@gmail.com>
2018-01-27 01:18:38 -0500

    Added and ugly header, button to pause/play, file upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 const THREE = require('three');
 
-let fftSize = 128;
+let fftSize = 64;
+
+let audioFile = "../assets/flags-feat-yuna.mp3";
+let isPlaying = false;
+
+let audioInput = document.getElementById("songFile");
+audioInput.onchange = (event) => {
+  let fileList = audioInput.files;
+  audioFile = URL.createObjectURL(fileList[0]);
+  audio.pause();
+  isPlaying = false;
+  audioLoader.load(audioFile, (buffer) => {
+    audio.setBuffer(buffer);
+    audio.setLoop(true);
+    audio.play();
+    isPlaying = true;
+  });
+}
+
+let playPause = document.getElementById("playPause");
+playPause.onclick = (event) =>{
+  event.preventDefault();
+  if(isPlaying){
+    audio.pause();
+    isPlaying = false;
+  }
+  else {
+    console.log("click");
+    audio.play();
+    isPlaying = true;
+  }
+}
+
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
@@ -8,7 +40,9 @@ let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement);
+
+let container = document.getElementById('container');
+container.appendChild(renderer.domElement);
 
 let audioLoader = new THREE.AudioLoader();
 
@@ -16,13 +50,15 @@ let listener = new THREE.AudioListener();
 
 let audio = new THREE.Audio(listener);
 
-audioLoader.load('../assets/flags-feat-yuna.mp3', (buffer) => {
+audioLoader.load(audioFile, (buffer) => {
   audio.setBuffer(buffer);
   audio.setLoop(true);
   audio.play();
+  isPlaying = true;
 });
 
 let analyser = new THREE.AudioAnalyser(audio, fftSize);
+
 
 let uniforms = {
   tAudioData: {
@@ -30,9 +66,13 @@ let uniforms = {
   }
 };
 
-let material = new THREE.ShaderMaterial({uniforms: uniforms, vertexShader: document.getElementById('vertexShader').textContent, fragmentShader: document.getElementById('fragmentShader').textContent});
+console.log(uniforms.tAudioData.value);
 
-let geometry = new THREE.PlaneBufferGeometry(1, 1);
+let material = new THREE.ShaderMaterial({uniforms: uniforms,
+  vertexShader: document.getElementById('vertexShader').textContent,
+  fragmentShader: document.getElementById('fragmentShader').textContent});
+
+let geometry = new THREE.PlaneBufferGeometry(1.5, 1);
 
 let mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
